c87b1df699f283a0f1e1bb12940012a3b06eb535
tyson <tyson7757@comcast.net>
2021-04-19 09:59:42 -0500

    aws

--- dist/remoteEntry.js
+++ dist/remoteEntry.js
@@ -36121,7 +36121,7 @@ module.exports = require("zlib");
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "";
+/******/ 		__webpack_require__.p = "https://raw.githubusercontent.com/module-federation/MicroLib-Example/pre-npm/dist";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/sharing */
